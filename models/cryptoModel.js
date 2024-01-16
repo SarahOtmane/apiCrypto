1c453df228129fc5f1355bc97dc4e2ae1bda0cd1
Sarah Otmane <sarahotmane@MacBook-Air-de-Sarah.local>
2024-01-16 10:06:19 +0100

    création des classes

---
+++ models/cryptoModel.js
@@ -0,0 +1,24 @@
+class Crypto{
+    
+}
+
+
+class Bitcoin extends Crypto{
+    constructor(){
+        this.id = 'bitcoin'
+    }
+}
+
+
+class Ethereum extends Crypto{
+    constructor(){
+        this.id = 'ethereum'
+    }
+}
+
+
+class Solana extends Crypto{
+    constructor(){
+        this.id = 'solana'
+    }
+}
