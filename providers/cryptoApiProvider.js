2bfca4ea20b3b1546b27ce22a7a6df840e3065b2
MaxRichet <max.richet@my-digital-school.org>
2024-01-16 15:59:42 +0100

    adpteur up

--- providers/cryptoApiProvider.js
+++ providers/cryptoApiProvider.js
@@ -1,9 +1,31 @@
 const axios = require("axios");
-
+let parseString = require('xml2js').parseString;
+const paparse = require("papaparse");
 const baseUrl = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Csolana&vs_currencies=usd';
+// const baseUrl = 'https://www.w3schools.com/xml/note.xml';
 
 exports.getAllcryptos = async () => {
-    let response = await axios.get(baseUrl);
-    console.log(response.data);
-    return response.data;
+    let response1 = await axios.get(baseUrl);
+    
+    fetch(baseUrl)
+    .then(response => {
+        const contentType = response.headers.get('Content-Type');
+        
+        if (contentType.includes('json')) {
+            return response1.data;
+            
+        } else if (contentType.includes('xml')) {
+            parseString(response1.data, function (err, result) {
+                console.log(result);
+                return result;
+            });
+        } else if (contentType.includes('csv')) {
+            console.log('La réponse est au format CSV');
+            return paparse.parse(response1);
+
+        } else {
+            console.log('Format inconnu');
+        }
+    })
+    .catch(error => console.error('Erreur :', error));
 }
